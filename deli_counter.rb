a9c9ce69dfe1420f1f8fee75094b397164be7ae0
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 18:18:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,5 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome, #{name}. You are number #{array.length} in line"
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end

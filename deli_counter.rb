7451daba32b7365ce4ad6eff662ce8b9513eebce
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 18:14:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(array)
   if array.length == 0
-    puts "The line is currently empty."
+    "The line is currently empty."
   else
     sentence = ["The line is currently:"]
     array.each_with_index {|name, index|
